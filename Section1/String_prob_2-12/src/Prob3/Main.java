df3b522a9e33fa2846746e7255831e28d6490d92
JH-S <skskfl5786@gmail.com>
2025-02-01 15:56:23 +0900

    3번 문장 속 단어 복습완료

--- Section1/String_prob_2-12/src/Prob3/Main.java
+++ Section1/String_prob_2-12/src/Prob3/Main.java
@@ -2,13 +2,50 @@ package Prob3;
 
 import java.util.Scanner;
 
-
 public class Main {
+    public void Solution(String str) {
+        String[] newStr = str.split(" ");
+        String answer = "";
+        // int len = newStr[0].length();
+        int len = Integer.MIN_VALUE;
+
+        for(String s : newStr) {
+            if(len < s.length()) {
+                len = s.length();
+                answer = s;
+            }
+        }
+
+        System.out.print(answer);
+    }
+
+    public void Solution2(String str) {
+        String answer = "";
+        int len = Integer.MIN_VALUE;
+
+        while(str.indexOf(" ") != -1) {
+            int idx = str.indexOf(" ");
+            String tmp = str.substring(0,idx);
+
+            if(len<tmp.length()) {
+                len = tmp.length();
+                answer = tmp;
+            }
+            str = str.substring(idx+1);
+        }
+        if(len < str.length()) {
+            len = str.length();
+            answer = str;
+        }
+        System.out.print(answer);
+    }
+
     public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
 
-
+//        T.Solution(str);
+        T.Solution2(str);
     }
 }
-/*
-스트링으로 문자열을 받은 다음 공백으로 파싱?
- */
