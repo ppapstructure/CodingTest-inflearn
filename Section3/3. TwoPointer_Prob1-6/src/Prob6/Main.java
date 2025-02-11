07d99917c854908098804b69d85e9fb2dec5baba
JH-S <skskfl5786@gmail.com>
2025-02-11 22:27:28 +0900

    3단원 6번 최대길이 연속 부분수열 복습완료

--- Section3/3. TwoPointer_Prob1-6/src/Prob6/Main.java
+++ Section3/3. TwoPointer_Prob1-6/src/Prob6/Main.java
@@ -3,14 +3,30 @@ package Prob6;
 import java.util.Scanner;
 
 public class Main {
+    public int solution(int n, int k,int[] arr) {
+        int answer = 0,lt=0;
+        int cnt = 0;
+        for(int rt = 0;rt<n;rt++) {
+            if(arr[rt] == 0) cnt++;
+            while(cnt > k) {
+                if(arr[lt] == 0) cnt--;
+                lt++;
+            }
+            answer = Math.max(answer, rt-lt+1);
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int k = sc.nextInt();
         int[] arr = new int[n];
         for(int i = 0;i<n;i++) {
-            arr[i] = i+1;
+            arr[i] = sc.nextInt();
         }
-        System.out.print(T.solution(n, arr));
+        System.out.print(T.solution(n, k,arr));
     }
 }
